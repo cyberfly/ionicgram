31d8ea7aebf951fd4ce823d8dc2acfccc8ef823a
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-02-21 20:47:11 +0800

    generate MediaServiceProvider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MediaServiceProvider } from '../providers/media-service/media-service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MediaServiceProvider
   ]
 })
 export class AppModule {}
