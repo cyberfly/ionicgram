0e59e8b25e78777d49667ad33277f47c3b6f51c4
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-02-21 20:51:59 +0800

    add sample function for MediaServiceProvider

--- src/providers/media-service/media-service.ts
+++ src/providers/media-service/media-service.ts
@@ -14,4 +14,25 @@ export class MediaServiceProvider {
     console.log('Hello MediaServiceProvider Provider');
   }
 
+  //get list of uploaded media from API
+
+  getMedia()
+  {
+
+  }
+
+  //get single media detail from API
+
+  getMediaDetail()
+  {
+
+  }
+
+  //upload media into server using API
+
+  storeMedia()
+  {
+    
+  }
+
 }
