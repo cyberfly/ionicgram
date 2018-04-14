827ca785d583030d2583fe5539cca38e2e85333b
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-04-14 21:56:23 +0800

    take picture using camera feature

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -27,4 +27,23 @@ export class CameraPage {
     console.log('ionViewDidLoad CameraPage');
   }
 
+  //open camera and take picture
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 70,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.image_uri = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
 }
