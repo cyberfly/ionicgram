163bd89c4413907a596c391bf021ec39154d725c
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-04-14 15:58:13 +0800

    change second tabs icon to camera and point to CameraPage

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
+import { CameraPage } from './../camera/camera';
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
@@ -10,7 +10,7 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = CameraPage;
   tab3Root = ContactPage;
 
   constructor() {
